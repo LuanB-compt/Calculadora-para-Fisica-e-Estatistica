d59a853863bdb1dc5d1b2797359ff5981a1d6021
Luan Bruno Domingues de Oliveira <80792614+LuanB-compt@users.noreply.github.com>
2021-04-07 18:23:09 -0300

    Update Hora do MRU.c

--- Hora do MRU.c
+++ Hora do MRU.c
@@ -4,81 +4,66 @@
 
 int main(void)
 {
+    do{
     setlocale(LC_ALL, "portuguese");
     system("color 75");
 
-    void tela_mru(x) //funcÁ„o que atualiza a tela junto com o menu
+    void tela_mru(x) //func√ß√£o que atualiza a tela junto com o menu para o programa MRU
     {
+        system("cls");
+        printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        printf("\n              MOVIMENTO RET√çLINEO UNIFORME");
+        printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
 
-        if(x == 0)
-        {
-            system("cls");
-            printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n              MOVIMENTO RETÕLINEO UNIFORME");
-            printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-        }
         if(x == 1)
         {
-            system("cls");
-            printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n              MOVIMENTO RETÕLINEO UNIFORME");
-            printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n***                ESPA«O PERCORRIDO                     ***");
+            printf("\n***                ESPA√áO PERCORRIDO                     ***");
         }
         if(x == 2)
         {
-            system("cls");
-            printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n              MOVIMENTO RETÕLINEO UNIFORME");
-            printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n***                  POSI«√O INICIAL                     ***");
+            printf("\n***                  POSI√á√ÉO INICIAL                     ***");
         }
         if(x == 3)
         {
-            system("cls");
-            printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n              MOVIMENTO RETÕLINEO UNIFORME");
-            printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
             printf("\n***                     VELOCIDADE                       ***");
         }
         if(x == 4)
         {
-            system("cls");
-            printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            printf("\n              MOVIMENTO RETÕLINEO UNIFORME");
-            printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
             printf("\n***                       TEMPO                          ***");
         }
     }
 
-    //variavel de cada do while,  que est· em todos os cases do switch
+
+
+    //variavel de cada do while,  que est√° em todos os cases do switch
     char continua_mru1;
     char continua_mru2;
     char continua_mru3;
     char continua_mru4;
 
-    tela_mru(0);
+    tela_mru(0);//printa o menu usando a fun√ß√£o criada tela_mru
 
-    //mostra as opÁıes de vari·veis que o programa pode calcular, e qual o usu·rio pode usar
-    int icognita;
-    printf("\n\n\n\nSe deseja encontrar o ESPA«O PERCORRIDO (S) pelo corpo ----> DIGITE 1");
-    printf("\nSe deseja encontrar a POSI«√O INICIAL (S0) do corpo ----> DIGITE 2");
+    //mostra as op√ß√µes de vari√°veis que o programa pode calcular, e qual o usu√°rio pode usar
+    int menu = -1;
+    printf("\n\n\n\nSe deseja encontrar o ESPA√áO PERCORRIDO (S) pelo corpo ----> DIGITE 1");
+    printf("\nSe deseja encontrar a POSI√á√ÉO INICIAL (S0) do corpo ----> DIGITE 2");
     printf("\nSe deseja encontrar a VELOCIDADE (V) do corpo ----> DIGITE 3");
     printf("\nSe deseja encontrar o TEMPO (t) do movimento ----> DIGITE 4");
+    printf("\nSe deseja sair ----> DIGITE 0");
     printf("\n\nINSIRA AQUI: ");
-    scanf("%d", & icognita);
+    scanf("%d", & menu);
 
-    //leva para cada c·lculo diferente
-    switch(icognita)
+    //leva para cada c√°lculo diferente
+    switch(menu)
     {
         case 1:
             do
             {
                 tela_mru(1);
 
-                //recebe a posiÁ„o inicial para a conta
+                //recebe a posi√ß√£o inicial para a conta
                 float posicao_inicial;
-                printf("\n\n\n\nInsira a POSI«√O INICIAL (S0) METROS do corpo: ");
+                printf("\n\n\n\nInsira a POSI√á√ÉO INICIAL (S0) METROS do corpo: ");
                 scanf("%f", & posicao_inicial);
 
                 //recebe a velocidade para a conta
@@ -91,7 +76,7 @@ int main(void)
                 printf("Insira o TEMPO (t) em SEGUNDOS que durou o movimento: ");
                 scanf("%f", & tempo);
 
-                //calcula o espaÁo percorrido se no mÌnimo o tempo e a velocidade forem diferentes que zero
+                //calcula o espa√ßo percorrido se no m√≠nimo o tempo e a velocidade forem diferentes que zero
                 float espaco_percorrido;
                 if(tempo != 0 && velocidade != 0)
                 {
@@ -99,26 +84,29 @@ int main(void)
                 }
                 else
                 {
-                    printf("\n\nAlgum valor deve ter sido colocado errado, tente novamente");
+                    printf("\n\nALGUM VALOR DEVE TER SIDO INSIRIDO ERRADO, ESPERE 5 SEGUNDOS E TENTE NOVAMENTE...");
+                    Sleep(5000);
+                    break;
                 }
 
-                printf("\n\nO ESPA«O PERCORRIDO (S) foi: %.4f\n", espaco_percorrido); //printa na tela o resultado
-                printf("F”RMULA: S = S0 + V * t\n"); //formula usada para o calculo
+                printf("\n\nO ESPA√áO PERCORRIDO (S) foi: %.4f\n", espaco_percorrido); //printa na tela o resultado
+                printf("F√ìRMULA: S = S0 + V * t\n"); //formula usada para o calculo
 
-                //termina ou comeÁa novamente o programa
+                //termina ou come√ßa novamente o programa
                 fflush(stdin);
-                printf("Deseja fazer uma nova mÈdia?(s/n) ");
+                printf("Deseja fazer uma nova m√©dia?(s/n) ");
                 scanf("%c", & continua_mru1);
             }while(continua_mru1 == 115);
+            break;
 
         case 2:
             do
             {
                 tela_mru(2);
 
-                //recebe o espaÁo percorrido para a conta
+                //recebe o espa√ßo percorrido para a conta
                 float espaco_percorrido;
-                printf("\n\n\n\nInsira ESPA«O PERCORRIDO (S) em METROS pelo corpo: ");
+                printf("\n\n\n\nInsira ESPA√áO PERCORRIDO (S) em METROS pelo corpo: ");
                 scanf("%f", & espaco_percorrido);
 
                 //recebe a velocidade para a conta
@@ -131,7 +119,7 @@ int main(void)
                 printf("Insira o TEMPO (t) em SEGUNDOS que durou o movimento: ");
                 scanf("%f", & tempo);
 
-                //calcula a posiÁ„o inicial se no mÌnimo o tempo e a velocidade forem diferentes que zero
+                //calcula a posi√ß√£o inicial se no m√≠nimo o tempo e a velocidade forem diferentes que zero
                 float posicao_inicial;
                 if(tempo != 0 && velocidade != 0)
                 {
@@ -139,31 +127,34 @@ int main(void)
                 }
                 else
                 {
-                    printf("\n\nAlgum valor deve ter sido colocado errado, tente novamente");
+                    printf("\n\nALGUM VALOR DEVE TER SIDO INSIRIDO ERRADO, ESPERE 5 SEGUNDOS E TENTE NOVAMENTE...");
+                    Sleep(5000);
+                    break;
                 }
 
-                printf("\n\nA POSI«√O INICIAL (S0) È: %.4f\n", posicao_inicial); //printa na tela o resultado
-                printf("F”RMULA: S0 = S - V * t\n"); //formula usada para o calculo
+                printf("\n\nA POSI√á√ÉO INICIAL (S0) √©: %.4f\n", posicao_inicial); //printa na tela o resultado
+                printf("F√ìRMULA: S0 = S - V * t\n"); //formula usada para o calculo
 
-                //termina ou comeÁa novamente o programa
+                //termina ou come√ßa novamente o programa
                 fflush(stdin);
-                printf("Deseja fazer uma nova mÈdia?(s/n) ");
+                printf("Deseja fazer uma nova m√©dia?(s/n) ");
                 scanf("%c", & continua_mru2);
             }while (continua_mru2 == 115);
+            break;
 
         case 3:
             do
             {
                 tela_mru(3);
 
-                //recebe o espaÁo percorrido para a conta
+                //recebe o espa√ßo percorrido para a conta
                 float espaco_percorrido;
-                printf("\n\n\n\nInsira ESPA«O PERCORRIDO (S) em METROS pelo corpo: ");
+                printf("\n\n\n\nInsira ESPA√áO PERCORRIDO (S) em METROS pelo corpo: ");
                 scanf("%f", & espaco_percorrido);
 
-                //recebe a posiÁ„o inicial para a conta
+                //recebe a posi√ß√£o inicial para a conta
                 float posicao_inicial;
-                printf("Insira a POSI«√O INICIAL (S0) METROS do corpo: ");
+                printf("Insira a POSI√á√ÉO INICIAL (S0) METROS do corpo: ");
                 scanf("%f", & posicao_inicial);
 
                 //recebe o tempo para a conta
@@ -171,7 +162,7 @@ int main(void)
                 printf("Insira o TEMPO (t) em SEGUNDOS que durou o movimento: ");
                 scanf("%f", & tempo);
 
-                //calcula a posiÁ„o inicial se no mÌnimo o tempo e a velocidade forem diferentes que zero
+                //calcula a posi√ß√£o inicial se no m√≠nimo o tempo e a velocidade forem diferentes que zero
                 float velocidade;
                 if(tempo != 0 && espaco_percorrido != 0)
                 {
@@ -179,21 +170,69 @@ int main(void)
                 }
                 else
                 {
-                    printf("\n\nAlgum valor deve ter sido colocado errado, tente novamente");
+                    printf("\n\nALGUM VALOR DEVE TER SIDO INSIRIDO ERRADO, ESPERE 5 SEGUNDOS E TENTE NOVAMENTE...");
+                    Sleep(5000);
+                    break;
                 }
 
-                printf("\n\nA VELOCIDADE (V) È: %.4f\n", velocidade); //printa na tela o resultado
-                printf("F”RMULA: V = (S - S0) / t\n"); //formula usada para o calculo
+                printf("\n\nA VELOCIDADE (V) √©: %.4f\n", velocidade); //printa na tela o resultado
+                printf("F√ìRMULA: V = (S - S0) / t\n"); //formula usada para o calculo
 
 
-                //termina ou comeÁa novamente o programa
+                //termina ou come√ßa novamente o programa
                 fflush(stdin);
-                printf("Deseja fazer uma nova mÈdia?(s/n) ");
+                printf("Deseja fazer uma nova m√©dia?(s/n) ");
                 scanf("%c", & continua_mru3);
             }while (continua_mru3 == 115);
+            break;
 
+        case 4:
+            do
+            {
+                tela_mru(4);//printa o menu
+
+                //recebe a posi√ß√£o inicial para a conta
+                float posicao_inicial;
+                printf("\n\n\n\nInsira a POSI√á√ÉO INICIAL (S0) METROS do corpo: ");
+                scanf("%f", & posicao_inicial);
+
+                //recebe o espa√ßo percorrido para a conta
+                float espaco_percorrido;
+                printf("Insira ESPA√áO PERCORRIDO (S) em METROS pelo corpo: ");
+                scanf("%f", & espaco_percorrido);
+
+                //recebe a velocidade para a conta
+                float velocidade;
+                printf("Insira a VELOCIDADE (V) em METROS POR SEGUNDO do corpo: ");
+                scanf("%f", & velocidade);
+
+                //calcula a posi√ß√£o inicial se no m√≠nimo o tempo e a velocidade forem diferentes que zero
+                float tempo;
+                if(velocidade != 0 && espaco_percorrido != 0)
+                {
+                    tempo = (posicao_inicial - espaco_percorrido) / velocidade;
+                }
+                else
+                {
+                    printf("\n\nALGUM VALOR DEVE TER SIDO INSIRIDO ERRADO, ESPERE 5 SEGUNDOS E TENTE NOVAMENTE...");
+                    Sleep(5000);
+                    break;
+                }
+
+                printf("\n\nA TEMPO (t) foi de: %.4f\n", tempo); //printa o resultado
+                printf("F√ìRMULA: t = (S0 - S) / V\n"); //formula usada para o calculo
+
+                //termina ou come√ßa novamente o programa
+                fflush(stdin);
+                printf("Deseja fazer uma nova m√©dia?(s/n) ");
+                scanf("%c", & continua_mru3);
+            }while (continua_mru4== 115);
+            break;
+
+        case 0:
+            return 0;
     }
-
+    }while(1);
 
     system("pause");
     return 0;
