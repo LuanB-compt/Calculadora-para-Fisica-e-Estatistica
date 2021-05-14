b3cc260554fea474e09d8cd496e93d0e398e1d9f
LuanB-compt <luan-bruno-domingues@hotmail.com>
2021-05-14 07:47:29 -0300

    trabalho no Desvio Padrao

--- Hora_DesvioPadrao.c
+++ Hora_DesvioPadrao.c
@@ -7,9 +7,9 @@ void titulo_desvio(x)
 {
     system("cls");
     fflush(stdin);
-    printf("96969696969696969696969696969696969696969696");
-    printf("\n                DESVIO PADRÃO");
-    printf("\n96969696969696969696969696969696969696969696");
+    printf("9696969696969696969696969696969696969696969696969696969696969696");
+    printf("\n                       DESVIO PADRÃO");
+    printf("\n9696969696969696969696969696969696969696969696969696969696969696");
     if(x == 1)
     {
         printf("\n                VARIÂNCIA");
@@ -51,12 +51,30 @@ int main(void)
 
                     float desvio_padrao;
                     float variancia;
-
+                    float variancia_quadrado;
+                    float media;
+                    float soma;
+                    float divisao_variancia;
+                    int n = 0;
                     if(ARRAYLEN(base_dados) > 1)
                     {
-                        variancia =
+                        for(i = 0; i < numero_elementos - 1; i++)
+                        {
+                            soma += base_dados[i];//soma os elementos
+                        }
+                        soma += base_dados[i - 1];//soma último elemento
 
-                        desvio_padrao = sqrt(variancia);
+                        media = soma / numero_elementos;//média dos elementos
+
+                        for(i = 0; i < numero_elementos; i++)
+                        {
+                            variancia = media - base_dados[i];//variancia
+                        }
+                        variancia_quadrado = pow(variancia, 2);//variancia ao quadrado
+
+                        divisao_variancia = variancia_quadrado / numero_elementos;//variancia ao quadrado por "n"
+
+                        desvio_padrao = sqrt(divisao_variancia);//raiz do resultado
                     }
 
                 }while(continua_desvio == 's');
