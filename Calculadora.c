683e27a95b051aebd6566faba7a5e0c1f5dd2515
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2021-05-22 21:20:38 -0300

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -14,24 +14,29 @@ int main(void)
         fflush(stdin);
         system("cls");
         system("color 0F");
+        printf("CALCULADORA\n\n\n");
         printf("1 --- Média, Moda e Mediana\n");
-        printf("2 --- Movimento Retilíneo Uniformemente\n");
-        printf("3 --- Movimento Retilíneo Uniformemente Variado\n");
-        printf("4 --- Desvio e Variancia\n");
+        printf("2 --- Média Ponderada\n");
+        printf("3 --- Movimento Retilíneo Uniformemente\n");
+        printf("4 --- Movimento Retilíneo Uniformemente Variado\n");
+        printf("5 --- Desvio e Variancia\n");
         printf("0 --- Sair\n");
-        printf("Digite sua opção: ");
+        printf("\nDigite sua opção: ");
         scanf("%d", &menu_principal);
         switch(menu_principal) {
         case 1:
             MMM();
             break;
         case 2:
-            MRU();
+            MP();
             break;
         case 3:
-            MRUV();
+            MRU();
             break;
         case 4:
+            MRUV();
+            break;
+        case 5:
             Desvio();
             break;
         case 0:
@@ -538,6 +543,7 @@ void titulo_mruv(x)
 
 int MRUV(void)
 {
+    system("color E0");
     setlocale(LC_ALL,"portuguese");//acentuação da lingua portuguesa
     do{
 
@@ -830,6 +836,7 @@ void titulo_desvio(x)
 
 int Desvio(void)
 {
+    system("color F0");
     setlocale(LC_ALL, "portuguese");
     do{
         titulo_desvio();
@@ -962,5 +969,124 @@ int Desvio(void)
 
     }while(1);
     system("pause");
+    return 0;
+}
+
+// ###############
+// MÉDIA PONDERADA
+// ###############
+
+void titulo_ponderada()
+    {
+        system("cls");
+        printf("------------------------------------------------------------------------------");
+        printf("\n***                           MÉDIA PONDERADA =)                           ***");
+        printf("\n------------------------------------------------------------------------------");
+        fflush(stdin);
+    }
+
+int MP(void)
+{
+    system("color E4");
+    setlocale(LC_ALL, "portuguese");
+    char continua;
+
+    do{
+        //título
+        titulo_ponderada();    //atualiza a tela
+        printf("\n\n\n");
+
+        //pede os dados do usuário
+        int tamanho;
+        float media_ponderada = 0;
+
+        printf("Quantos elementos? "); //pergunta com quantos elementos o usuário deseja fazer a média
+        scanf("%d", &tamanho);
+        printf("\n\n");
+
+        //conta dos pesos
+        float pesos_total = 0;
+        float pesos[tamanho];
+        for(int i = 0; i < tamanho; i++)
+        {
+            printf("Qual o peso do %i° elemento: ", i + 1);
+            scanf("%f", & pesos[i]); //recebe os pesos
+            fflush(stdin);
+            pesos_total += pesos[i]; //salva a soma de todos os pesos
+        }
+
+        //faz a proporção dos pesos com a soma total
+        for(int i = 0; i < tamanho; i++)
+        {   //não importa se a soma dos pesos tem de 100% ou menos que 100% (assim a proporção sempre é mantida)
+            pesos[i] = pesos[i] / pesos_total;
+        }
+
+        //recebe os dados
+        float elementos[tamanho];  //cria a lista com tamanho de elementos que o usuário deu
+        printf("\nDigite os dados: \n");
+        for (int i = 0; i < tamanho; i++)
+        {        //for para coletar dado por dado e adiciona-lo em seu respectivo espaço na lista
+            printf("Digite o %d°: ", i + 1);
+            scanf("%f", &elementos[i]);
+        }
+
+        titulo_ponderada();  //atualiza a tela
+        printf("\n\n\n");
+
+        //mostra os dados brutos inseridos pelo usuário
+        printf("Os elementos brutos são: ");
+        for (int i = 0; i < tamanho -1 ; i++)//colocado n-1 para não incluir o último elemento, pois o último elemento seja diferente
+        {
+            printf("%.2f; ", elementos[i]);    //printa os termos da lista
+        }
+        printf("%.2f.\n", elementos[tamanho-1]);   //formatação do último elemento,
+
+        //faz o calculo multiplicando cada elemento com seu devido peso
+        for (int i = 0; i < tamanho; i++)
+        {
+            /*printf("listinha: %.2f\n", elementos[i]);
+            printf("peso: %.2f\n", pesos[i]);*/
+            elementos[i] = elementos[i] * pesos[i];
+            //printf("%f", listinha[i]);
+        }
+
+        //pula linha
+        printf("\n\n\n");
+
+        //mostra os elementos já tratados depois da multiplicação
+        printf("Os elementos com suas proporções são: ");
+        for (int i = 0; i < tamanho -1 ; i++)//colocado n-1 para não incluir o último elemento, pois o último elemento seja diferente
+        {
+            printf("%.2f; ", elementos[i]);    //printa os termos da lista
+            media_ponderada += elementos[i];              //soma = soma + lista[indice]
+        }
+        media_ponderada += elementos[tamanho-1];              //soma do último elemento
+        printf("%.2f.\n", elementos[tamanho-1]);   //formatação do último elemento,
+        // essas duas coisas foram necessárias para que o último elemento fosse printado diferente
+
+        //calcula e mostra o tamanho da lista com os dados brutos
+        printf("São %d elementos.\n\n", tamanho);
+
+        printf("\n- Média = %.2f\n\n", media_ponderada);
+
+        //trava a tela para o usuário prosseguir a hora que ele quiser
+        char termina;
+        while (termina != 'c')
+        {
+            //system("cls");
+            fflush(stdin);
+            printf("\nDigíte C para continuar...\n");
+            termina = getch();
+        }
+        system("cls");
+
+        //termina ou começa novamente o programa
+        titulo_ponderada();
+        fflush(stdin);
+        printf("\n\nDeseja fazer uma nova média?(s/n) ");
+        scanf("%c", & continua);
+
+    }while (continua == 115);
+
     return 0;
 }
